0bb69d72c40d56a33a06b6105f08ce32c881803c
张靖 <zhangjing2@aixuedai.com>
2017-03-09 23:57:23 +0800

    change ice_server to ice_hello

--- ice_hello/src/main/java/cn/didadu/service/HelloWorldServiceImpl.java
+++ ice_hello/src/main/java/cn/didadu/service/HelloWorldServiceImpl.java
@@ -6,7 +6,7 @@ import cn.didadu.generate.helloworld._HelloWorldServiceDisp;
 /**
  * Created by zhangjing on 17-3-9.
  */
-public class HelloWorldServiceImpl extends  _HelloWorldServiceDisp {
+public class HelloWorldServiceImpl extends _HelloWorldServiceDisp {
     @Override
     public String hello(Current __current) {
         return "Hello World!";
